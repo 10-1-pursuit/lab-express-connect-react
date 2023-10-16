3b37bcf9c422862f95146ebe393e6bddd0e2e79e
xavierrice <xavier.rice@gmail.com>
2023-10-15 21:24:04 -0400

    added loading ternary for backed logs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div>Hello World</div>;
+  const [backEndLogs, setBackEndLogs] = useState([{}]);
+
+  useEffect(() => {
+    fetch(`${API}/logs`)
+      .then((response) => response.json())
+      .then((responseJSON) => setBackEndLogs(responseJSON))
+      .catch((error) => console.error(error));
+  }, []);
+
+  console.log(backEndLogs);
+
+  return (
+    <div>
+      { typeof backEndLogs.title === "undefined" ? (
+        <p>Loading...</p>
+      ) : (
+        backEndLogs.map((log, i) => <p key={i}>{log.title}</p>)
+      )}
+    </div>
+  );
 }
 
 export default App;
