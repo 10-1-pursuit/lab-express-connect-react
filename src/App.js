75de2cbd6b2d619706fbcd824f5e8a457e307b91
ShakaPursuit <shakalivngstone@pursuit.org>
2023-10-22 21:54:41 -0400

    index and show working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import { BrowserRouter as Router,Routes,Route, useParams } from "react-router-dom";
+import Header from "./Components/Header";
+import ShowTitles from "./Components/ShowTitles";
+import CaptainCard from "./Components/CaptainCard";
+// import { useParams } from 'react-router-dom';
+
+
+
+
 function App() {
-  return <div>Hello World</div>;
+
+  const {index}=useParams()
+ return (
+       <>
+<Router>
+    <Header/>
+       
+    <Routes>
+      <Route path="/logs" element={<ShowTitles/>}/>
+      <Route path="/logs/:index" element ={<CaptainCard/>}/>
+
+
+
+
+    </Routes>
+ </Router>
+ </>
+
+  
+ )
+  ;
 }
 
 export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,16 @@
+const Header=()=>{
+
+return(
+    <header><h1 id="head">Captains Log</h1>
+         <button id="new-Log">New Log</button>
+        </header>)
+
+
+
+
+
+
+
+}
+
+export default Header
