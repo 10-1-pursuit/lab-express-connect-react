cd26a049a4279c3bca344032d550302822d941ff
Kenneth-Parker <kennethkavon@gmail.com>
2023-10-24 01:33:23 -0400

    initial commit Hello World running

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-function App() {
-  return <div>Hello World</div>;
-}
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  );
+};
 
 export default App;
