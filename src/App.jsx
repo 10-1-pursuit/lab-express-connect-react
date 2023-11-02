d98c0a5dbd2ffe44c91d6aca31589d14f6277f82
Salomon Balthazar <131703377+tru-salomon@users.noreply.github.com>
2023-11-02 12:31:15 -0400

    initial frnt end setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
+
+function App() {
+  const [logs, setLogs] = useState([]) 
+
+  
+  
+  useEffect(() => {
+
+  }, [])
+
+  return <div>Hello World</div>;
+}
+
+export default App;
