1ebd9430f4da97b6077de38018c0dc7a64ec78d1
Salomon Balthazar <131703377+tru-salomon@users.noreply.github.com>
2023-11-06 10:57:14 -0500

    Small changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,24 @@ import React from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+const API = process.env.REACT_APP_API_URL;
+
+console.log(API)
 
 
 function App() {
-  const [logs, setLogs] = useState([]) 
+  const [logs, setLogs] = useState([])
 
-  
-  
   useEffect(() => {
-
+    axios.get(`${API}/logs`).then(res => setLogs(res.data)).catch(error => console.log(error))
   }, [])
 
-  return <div>Hello World</div>;
+  return (
+    <>
+      <div>Hello World</div>;
+      <div> {logs.map((log, index) => <div key={index}>{log.captainName}</div>)} </div>
+
+    </>)
 }
 
 export default App;
